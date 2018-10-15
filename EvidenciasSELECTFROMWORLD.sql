783c05241ab245d8804ccc5bbe21464b45d33950
Catherine Mendoza <36484718+cathymendoza@users.noreply.github.com>
2018-10-15 07:56:33 -0700

    Update EvidenciasSELECTFROMWORLD.sql

--- EvidenciasSELECTFROMWORLD.sql
+++ EvidenciasSELECTFROMWORLD.sql
@@ -1,50 +1,50 @@
 ## SELECT FROM WORLD
-Observe the result of running this SQL command to show the name, continent and population of all countries.
+-- #01.- Observe the result of running this SQL command to show the name, continent and population of all countries.
 SELECT name, continent, population FROM world
 
-Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+-- #02.- Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
 SELECT name FROM world
 WHERE population >= 200000000
 
-Give the name and the per capita GDP for those countries with a population of at least 200 million.
+-- #03.- Give the name and the per capita GDP for those countries with a population of at least 200 million.
 SELECT name, gdp/population
 From world
 WHERE population>200000000
 
-Divide the population by 1000000 to get population in millions.
+-- #04.- Divide the population by 1000000 to get population in millions.
 SELECT name, population/1000000 FROM world
 WHERE continent LIKE 'South America'
 
-Show the name and population for France, Germany, Italy.
+-- #05.- Show the name and population for France, Germany, Italy.
 SELECT name country, population
 FROM world
 WHERE name IN ('France', 'Germany', 'Italy')
  
-Show the countries which have a name that includes the word 'United'
+-- #06.- Show the countries which have a name that includes the word 'United'
 SELECT name FROM world
 Where name LIKE 'united%'
  
-Show the countries that are big by area or big by population. Show name, population and area.
+-- #07.- Show the countries that are big by area or big by population. Show name, population and area.
 SELECT name, population, area from world
 WHERE population>250000000 or area >3000000
 
-Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
+-- #08.- Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
 
-For South America show population in millions and GDP in billions both to 2 decimal places.
+-- #09.- For South America show population in millions and GDP in billions both to 2 decimal places.
 SELECT name, ROUND(population/1000000,2),
 ROUND(gdp/1000000000,2)
 From world
 Where continent = 'South America'
 
-Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+-- #10.- Show per-capita GDP for the trillion dollar countries to the nearest $1000.
 SELECT name, ROUND(gdp/population,-3)
   FROM world
   WHERE
   gdp>1000000000000
 
-Show the name and capital where the name and the capital have the same number of characters.
+-- #11.- Show the name and capital where the name and the capital have the same number of characters.
 
-Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+-- #12.- Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
 
-Find the country that has all the vowels and no spaces in its name.
+-- #13.- Find the country that has all the vowels and no spaces in its name.
 
