e39b7cfffbea986a7cc18fa776b7a6979c07d3bb
Catherine Mendoza <36484718+cathymendoza@users.noreply.github.com>
2018-10-15 08:02:28 -0700

    Update EvidenciasSELECTBASICS.sql

--- EvidenciasSELECTBASICS.sql
+++ EvidenciasSELECTBASICS.sql
@@ -2,10 +2,13 @@
 SELECT population FROM world
      WHERE name = 'Germany'
 
+
 -- #02.- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 SELECT name, population FROM world
   WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
+
 -- #03.- Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
+
