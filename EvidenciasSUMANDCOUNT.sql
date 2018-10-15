8583a34237d1a2e48230b861ed627cf5f62abb7d
Catherine Mendoza <36484718+cathymendoza@users.noreply.github.com>
2018-10-15 07:54:39 -0700

    Update EvidenciasSUMANDCOUNT.sql

--- EvidenciasSUMANDCOUNT.sql
+++ EvidenciasSUMANDCOUNT.sql
@@ -1,6 +1,30 @@
 # SQLZooMendozaMtz
 
--- # 01.- Show the total population of the world.
+-- #01.- Show the total population of the world.
 SELECT SUM(population)
 FROM world;
+
+-- #02.- List all the continents - just once each.
+SELECT DISTINCT continent
+FROM world;
+
+-- #03.- List all the continents - just once each.
+
+
+-- #04.- How many countries have an area of at least 1000000
+
+
+-- #05.- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+
+
+-- #06.- For each continent show the continent and number of countries.
+
+
+
+-- #07.- For each continent show the continent and number of countries with populations of at least 10 million.
+
+
+
+-- #08.- List the continents that have a total population of at least 100 million.
+
 
