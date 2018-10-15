99decd4ca574dcf8b1d4fc6d8aa8c6934ee553d7
Catherine Mendoza <36484718+cathymendoza@users.noreply.github.com>
2018-10-15 08:25:32 -0700

    Update EvidenciasNOBEL.sql

--- EvidenciasNOBEL.sql
+++ EvidenciasNOBEL.sql
@@ -47,21 +47,31 @@ WHERE winner LIKE 'John%'
 -- #08.- Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
 SELECT yr, subject, winner FROM nobel
 WHERE subject = 'Physics' AND yr = 1980 OR
-subject = 'Chemistry' AND yr = 1984;
+subject = 'Chemistry' AND yr = 1984
 
 
 -- #09.- Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
 SELECT * FROM nobel
-WHERE yr = 1980 AND subject <> 'Chemistry' AND subject <> 'Medicine';
+WHERE yr = 1980 AND subject <> 'Chemistry' AND subject <> 'Medicine'
 
 
 -- #10.- Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+SELECT * FROM nobel
+WHERE yr < 1910 AND subject = 'Medicine' OR
+yr >= 2004 AND subject = 'Literature'
+
 
 -- #11.- Find all details of the prize won by PETER GRÜNBERG
+SELECT * FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
 
-#-- 12.- Find all details of the prize won by EUGENE O'NEILL
+
+# -- 12.- Find all details of the prize won by EUGENE O'NEILL
+SELECT * FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
 
 -- #13.- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+
 
 -- #14.- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
 
