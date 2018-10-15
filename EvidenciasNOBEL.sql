f7939b2b8ef819e9831bddd0aad01c153a3da7a6
Catherine Mendoza <36484718+cathymendoza@users.noreply.github.com>
2018-10-15 08:00:53 -0700

    Update EvidenciasNOBEL.sql

--- EvidenciasNOBEL.sql
+++ EvidenciasNOBEL.sql
@@ -1,1 +1,14 @@
-# SQLZooMendozaMtz
+-- #01.- Change the query shown so that it displays Nobel prizes for 1950.
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+ 
+ -- #02.-Show who won the 1962 prize for Literature.
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+   
+-- #03.-Show who won the 1962 prize for Literature.
+
+
